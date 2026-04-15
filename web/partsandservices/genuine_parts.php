baa477f3b8ae5f0bbb9ac3db490f7d4e53d79756
Shane Mendoza <shanenorwin.mendoza@tup.edu.ph>
2026-04-15 17:24:44 +0800

    parts/service navbar fix

--- web/partsandservices/genuine_parts.php
+++ web/partsandservices/genuine_parts.php
@@ -7,8 +7,10 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
 
-
+<!-- Global CSS -->
+<link rel="stylesheet" href="/citimotorsweb/web/global.css">
 <style>
 
     .text-muted {
